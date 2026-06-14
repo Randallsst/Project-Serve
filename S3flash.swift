7827846ae62488db33638fb91360cb2d44bbaa5f
Lauren <lauren_ng_sze_min@s2025.ssts.edu.sg>
2026-06-14 09:47:44 +0800

    Hello settings page

---
+++ S3flash.swift
@@ -0,0 +1,18 @@
+//
+//  S3flash.swift
+//  Chem app
+//
+//  Created by Lauren Ng Sze Min on 14/6/26.
+//
+
+import SwiftUI
+
+struct S3flash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    S3flash()
+}
