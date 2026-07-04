bb49fdf31f99be4022bdb6e9a55b13b9e98f347a
Lauren <lauren_ng_sze_min@s2025.ssts.edu.sg>
2026-07-04 17:52:49 +0800

    Flashcards complete and errors within the file fixed, thx randall

---
+++ SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Lauren Ng Sze Min on 29/6/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
