4ab913a52d434b8055339ad2a10c815f3c744da9
Lauren <lauren_ng_sze_min@s2025.ssts.edu.sg>
2026-07-05 19:04:55 +0800

    TIPS

---
+++ ExamTip.swift
@@ -0,0 +1,18 @@
+//
+//  ExamTip.swift
+//  test
+//
+//  Created by Lauren Ng Sze Min on 4/7/26.
+//
+
+import SwiftUI
+
+struct ExamTip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExamTip()
+}
