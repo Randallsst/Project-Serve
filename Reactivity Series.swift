0add7d9278e8720d306d59ae4e6f9c6d0f291760
LNdaCuber <kirbyiscool12349@gmail.com>
2026-07-13 21:59:08 +0800

    Color scheme

--- Reactivity Series.swift
+++ Reactivity Series.swift
@@ -5,13 +5,12 @@
 //  Created by Leo Andreas Mansukhani on 8/6/26.
 //
 
+
 import SwiftUI
 
 struct Reactivity_Series: View {
     var body: some View {
-        ZStack {
-            Color(red: 119/255, green: 178/255, blue: 175/255)
-                .ignoresSafeArea()
+        
             VStack {
                 Text("Reactivity series")
                     .font(.title)
@@ -26,7 +25,7 @@ struct Reactivity_Series: View {
                         .padding(.horizontal)
                 }
             }
-        }
+        
     }
 }
 
